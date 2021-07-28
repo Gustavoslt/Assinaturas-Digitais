23632094c1f85360bc5b1fe426d4fa40a748fb94
“Gustavo <gustavoslt@hotmail.com>
2021-07-28 13:42:12 -0300

    Configurações iniciais e criação dos containers Docker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,10 @@
                     Laravel
                 </div>
 
+                <div id="app">
+                    
+                </div>
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
